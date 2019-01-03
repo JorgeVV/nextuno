fae396d0d3fbbff6f40a006bb3b64b74ff19106f
Jorge Villalobos <jorge.villalobos2809@gmail.com>
2019-01-03 03:05:26 -0400

    Add Webpack Bundle Analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 const withPlugins = require("next-compose-plugins");
 const withTypescript = require("@zeit/next-typescript");
+const withBundleAnalyzer = require("@zeit/next-bundle-analyzer");
 
-module.exports = withPlugins([withTypescript]);
+const bundleAnalyzerConfig = {
+  analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
+  analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
+  bundleAnalyzerConfig: {
+    server: {
+      analyzerMode: "static",
+      reportFilename: "../bundles/server.html"
+    },
+    browser: {
+      analyzerMode: "static",
+      reportFilename: "../bundles/client.html"
+    }
+  }
+};
+
+module.exports = withPlugins([
+  [withTypescript],
+  [withBundleAnalyzer, bundleAnalyzerConfig]
+]);
